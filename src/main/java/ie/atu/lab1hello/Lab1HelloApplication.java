e1e74d15dd8f92c385fd64141ad14c7a9f1e4874
Gavin Walsh <G00402296@atu.ie>
2023-10-03 15:20:36 +0100

    Commit 1 all files

---
+++ src/main/java/ie/atu/lab1hello/Lab1HelloApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab1hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1HelloApplication.class, args);
+    }
+
+}
