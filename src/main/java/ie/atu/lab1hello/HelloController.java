d31a11801e17cbcc2b55d0db4673f7cca6828ea0
Gavin Walsh <G00402296@atu.ie>
2023-10-03 15:31:11 +0100

    Final Commit

---
+++ src/main/java/ie/atu/lab1hello/HelloController.java
@@ -0,0 +1,14 @@
+package ie.atu.lab1hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+}
